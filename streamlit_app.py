1e778fc5779c15c704803e0d157f5f1cbb182d53
charu1623 <153809804+charu1623@users.noreply.github.com>
2023-12-14 16:59:54 +0530

    Update streamlit_app.py
    added smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado-toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
   
